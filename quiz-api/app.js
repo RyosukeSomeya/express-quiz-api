a5405a67764e73f5dd4952eb95b8a5721b55c640
RyosukeSomeya <rx.78.0618@gmail.com>
2021-09-22 00:46:33 +0900

    フロントエンド追加、Expressへcors対応追加

--- quiz-api/app.js
+++ quiz-api/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const apiRouter = require('./routes/index');
 
@@ -17,6 +18,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors({
+  origin: 'http://127.0.0.1:5500',
+  credentials: true,
+  optionsSuccessStatus: 200
+}));
+
+// require cors　しない場合
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.use('/api/v1/quiz/', apiRouter);
 
